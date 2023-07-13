ea3206060b5516450b5335c52653af4057c6018b
writeitinc <writeitinc99@gmail.com>
2023-07-13 14:44:43 -0600

    add maggot_div_floor(),_ceil(),_round()

--- src/maggot/maggot.h
+++ src/maggot/maggot.h
@@ -1,6 +1,7 @@
 #ifndef maggot_h
 #define maggot_h
 
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
 
@@ -20,6 +21,11 @@ static inline MaggotStatus maggot_sub(size_t a, size_t b, size_t *ret_res);
 static inline MaggotStatus maggot_mult(size_t a, size_t b, size_t *ret_res);
 static inline MaggotStatus maggot_div(size_t a, size_t b,
 		MaggotDivResult *ret_res);
+static inline MaggotStatus maggot_div_floor(size_t a, size_t b,
+		size_t *ret_res);
+static inline MaggotStatus maggot_div_ceil(size_t a, size_t b, size_t *ret_res);
+static inline MaggotStatus maggot_div_round(size_t a, size_t b,
+		size_t *ret_res);
 
 static inline MaggotStatus maggot_add(size_t a, size_t b, size_t *ret_res)
 {
@@ -67,6 +73,51 @@ static inline MaggotStatus maggot_div(size_t a, size_t b,
 		.quot = quot,
 		.rem = rem
 	};
+	return MAGGOT_OK;
+}
+
+static inline MaggotStatus maggot_div_floor(size_t a, size_t b, size_t *ret_res)
+{
+	if (b == 0) {
+		return MAGGOT_DIV_BY_ZERO;
+	}
+
+	size_t quot_floor = a / b;
+
+	*ret_res = quot_floor;
+	return MAGGOT_OK;
+}
+
+static inline MaggotStatus maggot_div_ceil(size_t a, size_t b, size_t *ret_res)
+{
+	if (b == 0) {
+		return MAGGOT_DIV_BY_ZERO;
+	}
+
+	size_t quot = a / b;
+	size_t rem = a % b;
+
+	size_t quot_ceil = quot + (rem > 0);
+
+	*ret_res =  quot_ceil;
+	return MAGGOT_OK;
+}
+
+static inline MaggotStatus maggot_div_round(size_t a, size_t b, size_t *ret_res)
+{
+	if (b == 0) {
+		return MAGGOT_DIV_BY_ZERO;
+	}
+
+	size_t quot = a / b;
+	size_t rem = a % b;
+
+	bool b_is_odd = b % 2 == 1;
+	bool round_up = rem >= (b / 2 + b_is_odd);
+
+	size_t quot_rounded = quot + round_up;
+
+	*ret_res = quot_rounded;
 	return MAGGOT_OK;
 }
 
